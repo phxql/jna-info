f0c4516e1fbd8ee042707daceffa8fee4d689ae6
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-01-19 20:48:02 +0100

    Add test invoke with JNA

--- src/main/java/de/mkammerer/jnainfo/Program.java
+++ src/main/java/de/mkammerer/jnainfo/Program.java
@@ -1,11 +1,22 @@
 package de.mkammerer.jnainfo;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import com.sun.jna.PlatformExt;
 
 public class Program {
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.loadLibrary(Platform.isWindows() ? "msvcrt" : "c", CLibrary.class);
+
+        void printf(String format, Object... args);
+    }
+
     public static void main(String[] args) {
         String nativeLibraryResourcePrefix = PlatformExt.getNativeLibraryResourcePrefix();
-
         System.out.println("nativeLibraryResourcePrefix: " + nativeLibraryResourcePrefix);
+
+        System.out.print("Testing JNA... ");
+        CLibrary.INSTANCE.printf("success\n");
     }
 }
