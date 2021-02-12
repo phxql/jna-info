3892e2947bbf8477980d321b58bf61e13a000799
Moritz Kammerer <moritz.kammerer@qaware.de>
2021-02-12 09:06:59 +0100

    Refactor code, clean up gradle build

--- src/main/java/de/mkammerer/jnainfo/Program.java
+++ src/main/java/de/mkammerer/jnainfo/Program.java
@@ -3,22 +3,21 @@ package de.mkammerer.jnainfo;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
-import com.sun.jna.PlatformExt;
 
 public class Program {
     public interface CLibrary extends Library {
-        CLibrary INSTANCE = Native.load(Platform.isWindows() ? "msvcrt" : "c", CLibrary.class);
-
         void printf(String format, Object... args);
     }
 
     public static void main(String[] args) {
         System.out.println("JNA version: " + Native.VERSION);
+        System.out.println("Prefix: " + Platform.RESOURCE_PREFIX);
+        System.out.println("Arch: " + Platform.ARCH);
+        System.out.println();
 
-        String nativeLibraryResourcePrefix = PlatformExt.getNativeLibraryResourcePrefix();
-        System.out.println("nativeLibraryResourcePrefix: " + nativeLibraryResourcePrefix);
 
         System.out.print("Testing JNA... ");
-        CLibrary.INSTANCE.printf("success\n");
+        CLibrary instance = Native.load(Platform.C_LIBRARY_NAME, CLibrary.class);
+        instance.printf("success\n");
     }
 }
